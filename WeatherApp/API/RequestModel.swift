ba982361ab6c45bbeb9bafd9a15e55cde359f273
Dans Domanevskis <dandomanevski@gmail.com>
2021-01-11 20:07:12 +0200

    API update

--- WeatherApp/API/RequestModel.swift
+++ WeatherApp/API/RequestModel.swift
@@ -11,16 +11,40 @@ struct Weather: Codable {
     let cod: String
     let message: String
     let cnt: Float
+    let list: CurrentWeather
+    let city: CityWeather
 }
 
 struct CurrentWeather: Codable {
     let dt: Float
-    let weather: Float
-    let clouds: Float
-    let wind: Float
+    let main: CurrentWeatherMain
+    let weather: WeatherWeather
+    let clouds: CurrentClouds
+    let wind: CurrentWind
     let visibility: Float
     let pop: Float
+    let sys: CurrentSys
     let dt_txt: String
+}
+
+struct CurrentClouds: Codable {
+    let all: Float
+}
+
+struct CurrentWind: Codable {
+    let speed: Float
+    let deg: Float
+}
+
+struct CurrentSys: Codable {
+    let pod: String
+}
+
+struct WeatherWeather: Codable {
+    let id: Float
+    let main: String
+    let description: String
+    let icon: String
 }
 
 struct CurrentWeatherMain: Codable {
@@ -32,15 +56,21 @@ struct CurrentWeatherMain: Codable {
     let sea_level: Float
     let grnd_level: Float
     let humidity: Float
-    let tamp_kf: Float
+    let temp_kf: Float
 }
 
 struct CityWeather: Codable {
     let id: Float
     let name: String
+    let coord: CityCoord
     let country: String
     let population: Float
     let timezone: Float
     let sunrise: Float
     let sunset: Float
+}
+
+struct CityCoord: Codable {
+    let lat: Float
+    let lon: Float
 }
